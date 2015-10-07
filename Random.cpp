d6c12edbf3fcf96104b7a8b494a054148a3c6ba3
Moose <moose@posteo.de>
2015-10-07 15:27:10 +0200

    random uuid functionality

--- Random.cpp
+++ Random.cpp
@@ -10,16 +10,19 @@
 #include <boost/chrono/chrono.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+#include <boost/uuid/uuid_generators.hpp>
 
 namespace moose {
 namespace tools {
 
 namespace {
-
-	boost::thread_specific_ptr<boost::random::mt19937> local_gen;
+	
+	typedef boost::random::mt19937                             prng_type;
+	boost::thread_specific_ptr<prng_type>                      local_gen;
+	boost::thread_specific_ptr<boost::uuids::random_generator> random_uid_generator;
 	
 	/// get access to a thread local instance of the PRNG
-	inline boost::random::mt19937* gen() {
+	inline prng_type* gen() {
 		
 		if (!local_gen.get()) {
 			
@@ -28,14 +31,26 @@ namespace {
 			boost::chrono::high_resolution_clock::time_point start = boost::chrono::high_resolution_clock::now();
 			unsigned int tid = faked_thread_id(); // this should take long enough...
 			boost::chrono::nanoseconds ns = boost::chrono::high_resolution_clock::now() - start;
-			local_gen.reset(new boost::random::mt19937(ns.count() % tid));
+			local_gen.reset(new prng_type(ns.count() % tid));
 		}
 		
 		return local_gen.get();
 	}
+	
+	// get access to a thread local instance of a uuid generator
+	inline boost::uuids::random_generator *get_uuid_generator(void) {
+		
+		if (!random_uid_generator.get()) {
+			prng_type *prng = gen();
+			random_uid_generator.reset(new boost::uuids::random_generator(*prng));
+		}
+		
+		return random_uid_generator.get();
+	}
+	
 }
 
-boost::uint64_t urand(const boost::uint64_t n_max) throw () {
+boost::uint64_t urand(const boost::uint64_t n_max) {
 
 	// get the thread local PRNG
 	boost::random::mt19937 *prng = gen();
@@ -43,6 +58,17 @@ boost::uint64_t urand(const boost::uint64_t n_max) throw () {
 	boost::random::uniform_int_distribution<> dist(0, n_max);
 	return dist(*prng);
 }
+
+boost::uint64_t urand(void) {
+
+	return moose::tools::urand(std::numeric_limits<boost::uint64_t>::max());
+}
+
+boost::uuids::uuid ruuid(void) {
+
+	boost::uuids::random_generator *gen = get_uuid_generator();
+	return (*gen)();
+};
 
 }
 }
--- Random.hpp
+++ Random.hpp
@@ -7,14 +7,28 @@
 #include "MooseToolsConfig.hpp"
 
 #include <boost/cstdint.hpp>
+#include <boost/uuid/uuid.hpp>
 
 namespace moose {
 namespace tools {
 
 /*! \brief thread safe random number shortcut
  * This will give you a thread safe random number between 0 and n_max
+ *  \throw std::bad_alloc when out of memory on first use
  */
-MOOSE_TOOLS_API boost::uint64_t urand(const boost::uint64_t n_max) throw ();
+MOOSE_TOOLS_API boost::uint64_t urand(const boost::uint64_t n_max);
+
+/*! \brief thread safe random number shortcut
+ * This will give you a thread safe random number between 0 and std::numerical_limits<uint64_t>::max()
+ *  \throw std::bad_alloc when out of memory on first use
+ */
+MOOSE_TOOLS_API boost::uint64_t urand();
+
+/*! \brief creates a random uuid out of thin air
+ *  \return random uuid
+ *  \throw std::bad_alloc when out of memory on first use
+ */
+MOOSE_TOOLS_API boost::uuids::uuid ruuid(void);
 
 }
 }
