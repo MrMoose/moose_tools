52510827d4f26aaf182fc11642a5bede0f306236
Moose <moose@posteo.de>
2017-02-24 15:11:17 +0100

    Possibility to link parent for incarnation increases

--- Carne.hpp
+++ Carne.hpp
@@ -13,17 +13,31 @@
 namespace moose {
 namespace tools {
 
+class IncarnatedUnusedParent {};
+
 /*! \brief Give a class an atomic incarnation counter 
- * 
+ *  
+ *  You may also provide a parent, causing an incarnation increase of this to also increase the parent object's.
  */
-template< typename DerivedType>
+template< typename DerivedType, typename ParentType = IncarnatedUnusedParent >
 class Incarnated {
+
+	// This container only works for types that are IdTagged
+	//BOOST_STATIC_ASSERT(boost::is_base_of< Incarnated< ParentType >, ParentType>::value);
 
 	protected:
 		//! Note that this c'tor can throw but only std::bad_alloc, which all new can
 		//! \throw std::bad_alloc when out of memory on first use
 		Incarnated(void)
 				: m_incarnation(0) {
+		}
+
+		//! Note that this c'tor can throw but only std::bad_alloc, which all new can
+		//! \throw std::bad_alloc when out of memory on first use
+		Incarnated(const ParentType *n_parent)
+			: m_incarnation(0)
+			, m_parent(n_parent) {
+
 		}
 
 		/*! \brief you can also give in the ID of course but only in protected c'tor
@@ -36,6 +50,15 @@ class Incarnated {
 
 		~Incarnated(void) noexcept = default;
 
+		/*! \brief hand in a parent object.
+			Without it the class will assert when increasing inc
+		 */
+		void set_parent(const ParentType *n_parent) {
+
+			m_parent(n_parent);
+			assert(m_parent);
+		}
+
 	public:
 		//! this is the main self-explanatory getter
 		boost::uint64_t incarnation(void) const noexcept {
@@ -43,9 +66,10 @@ class Incarnated {
 			return m_incarnation.load();
 		}
 
+		/// implementation to increase incarnation chooses if the parent will be increased as well
 		void increase_incarnation() noexcept {
 		
-			++m_incarnation;
+			return increase_incarnation_impl(std::is_same<ParentType, IncarnatedUnusedParent>::type());
 		}
 	
 		bool changed(const boost::uint64_t n_known_incarnation) const noexcept {
@@ -54,7 +78,25 @@ class Incarnated {
 		}
 
 	private:
+
+		//! This implementation is for classes that didn't specify a parent type
+		inline void increase_incarnation_impl(std::true_type) noexcept {
+
+			++m_incarnation;
+		}
+
+		//! This implementation is for classes which did specify a parent type
+		inline void increase_incarnation_impl(std::false_type) noexcept {
+
+			assert(m_parent);
+
+			++m_incarnation;
+			m_parent->increase_incarnation();
+		}
+
 		boost::atomic<boost::uint64_t>  m_incarnation;
+
+		ParentType               *m_parent = nullptr;
 };
 
 
